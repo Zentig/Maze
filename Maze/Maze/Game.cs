8a93600f6269b7b9547bd645a7f2d9d09ac3bce1
vhavunka <vhavunka@gmail.com>
2023-09-07 20:34:01 +0300

    First map

---
+++ Maze/Maze/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze
+{
+    public class Game
+    {
+        public static string[] Map =
+        { "XXXXXXXXXXXX",
+          "XXX  XXXXXXX",
+          "XXX  XXXXXXX",
+          "XXX       XX",
+          "XXXXXXXX  XX",
+          "XXXXXXXX  XX",
+          "XXXXXXXX  XX",
+          "XXXXXXXX  XX",
+          "XXXXXXXX  XX",
+          "XXX       XX",
+          "XXX  XXXXXXX",
+          "XXXXXXXXXXXX",
+        };
+
+        static void Main(string[] args)
+        {
+            InitMap();
+            Console.ReadKey();
+
+            void GameLoop()
+            {
+                while (true)
+                {
+                
+                }
+            }
+            void InitMap()
+            {
+                foreach (var item in Map)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
